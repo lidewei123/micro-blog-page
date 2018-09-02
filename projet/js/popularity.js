3cb92ed54f76a92731a0abe0ac02d321dc98c913
Your Name <email@example.com>
2018-09-03 03:02:12 +0800

    q

---
+++ projet/js/popularity.js
@@ -0,0 +1,8 @@
+function changeTab(tabCon_num){
+for(i=0;i<=4;i++) { 
+ document.getElementById("tabCon_"+i).style.display="none"; //将所有的层都隐藏 
+ } 
+ document.getElementById("tabCon_"+tabCon_num).style.display="block";//显示当前层 
+} 
+
+
